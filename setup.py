04c6a70a4f7fda3fc0cd4c2b855905b82956b766
David Fialho <fialho.david@protonmail.com>
2017-09-15 16:55:03 +0100

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mutual-debt-simplifier',
+    version='1.0',
+    description='Tool to perform mutual debt simplification',
+    url='https://github.com/ric2b/Mutual_Debt_Simplification',
+    license='MIT',
+    author='',
+    author_email='',
+
+    packages=find_packages(exclude='*.tests'),
+
+    install_requires=['docopt==0.6.2', 'graphviz==0.8'],
+
+    extras_require={
+        'test': ['unittest'],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'simplify-debts=mutual_debt.main:main',
+        ],
+    }
+)
